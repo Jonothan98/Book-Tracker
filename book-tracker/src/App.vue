61d07b94b1177211cdb7d39f71a28d4389a17d19
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2025-04-08 15:26:29 -0500

    feature: created file for Landing.vue and imported it into App.vue

--- book-tracker/src/App.vue
+++ book-tracker/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue';
 </script>
 
 <template>
   <header>
     <HelloWorld msg="You did it!" />
   </header>
+  <main>
+    <Landing/>
+  </main>
 </template>
---
+++ book-tracker/src/components/Landing.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>Hello this is landing</p>
+</template>
